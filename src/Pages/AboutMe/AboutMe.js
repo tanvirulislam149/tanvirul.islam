07678dcd3f075db7b60be92960a34e916787094d
tanvir <tanvirulislam149@gmail.com>
2022-07-11 13:26:25 +0600

    about me final edited

--- src/Pages/AboutMe/AboutMe.js
+++ src/Pages/AboutMe/AboutMe.js
@@ -30,7 +30,8 @@ const AboutMe = () => {
       </div>
       <div className="md:w-5/12 md:ml-48 py-20">
         <div className="h-full p-56 bg-green-500 invisible md:visible absolute top-0 right-0"></div>
-        <img style={{border: "16px white solid"}} className="md:absolute md:w-4/12 px-0" src="https://i.ibb.co/D7msbHw/aboutme.jpg" alt="" />
+        <div className="w-full p-36 bg-green-500 visible md:invisible absolute left-0 bottom-0 "></div>
+        <img style={{border: "16px white solid"}} className="relative md:absolute md:w-4/12 px-0" src="https://i.ibb.co/D7msbHw/aboutme.jpg" alt="" />
       </div>
     </div>
   );
