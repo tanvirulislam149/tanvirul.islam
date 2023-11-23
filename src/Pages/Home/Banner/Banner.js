8d30850bc11c796cb56f3872399ca9a4a1523573
tanvir <tanvirulislam149@gmail.com>
2023-11-23 18:50:37 +0600

    clip path added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -9,30 +9,31 @@ const Banner = () => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 1.2 }}
-      className="banner-bg md:pt-40 pt-32 pl-2 pr-6 lg:flex justify-between lg:px-10 banner-font"
+      className="banner-font relative"
     >
-      <div>
-        {/* <p className="text-2xl md:text-6xl">HELLO, I AM</p> */}
-        <p className="text-7xl md:text-8xl lg:text-9xl text-black">
-          TANVIRUL
-        </p>
-        <p className="text-7xl md:text-8xl lg:text-9xl text-black">
-          ISLAM
-        </p>
-        <p className="text-2xl md:text-5xl">MERN STACK DEVELOPER</p>
-        <a
-          type="button"
-          data-mdb-ripple="true"
-          data-mdb-ripple-color="light"
-          className="text-lg inline-block px-6 my-4 nav-transition pt-3 pb-2 bg-black hover:text-black text-white font-medium leading-tight uppercase rounded shadow-md hover:bg-white hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out active:bg-sky-500"
-          href="https://drive.google.com/file/d/1xtZt-wI5v3mjx1y_tmLLntqgv5fLlkC7/view?usp=sharing"
-          target={"blank"}
-        >
-          My Resume
-        </a>
+      <div className="banner-img sm:w-4/6 md:pt-40 pt-32 pl-2 pr-6 lg:px-10">
+        <div className="w-4/6">
+          <p className="text-7xl md:text-8xl xl:text-9xl text-black">
+            TANVIRUL
+          </p>
+          <p className="text-7xl md:text-8xl xl:text-9xl text-black">
+            ISLAM
+          </p>
+          <p className="text-2xl md:text-5xl">MERN STACK DEVELOPER</p>
+          <a
+            type="button"
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+            className="text-lg inline-block px-6 my-4 nav-transition pt-3 pb-2 bg-black hover:text-black text-white font-medium leading-tight uppercase rounded shadow-md hover:bg-white hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out active:bg-sky-500"
+            href="https://drive.google.com/file/d/1xtZt-wI5v3mjx1y_tmLLntqgv5fLlkC7/view?usp=sharing"
+            target={"blank"}
+          >
+            My Resume
+          </a>
+        </div>
       </div>
-      <div className="flex justify-end pt-10 md:pt-0 xl:pr-20">
-        <div>
+      <div className="flex justify-end xl:pr-20 md:pt-40 pt-44 pl-2 pr-6 lg:px-10 banner-right-img absolute top-60 sm:top-0 right-0 sm:w-7/12 ml-auto">
+        <div className="sm:w-4/6 md:w-auto">
           <div className="pt-10 md:w-96 text-right">
             <p className="text-4xl md:text-6xl pr-20">Hello!!</p>
             <p className="text-5xl pr-10">.........</p>
